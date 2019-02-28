6772231251cfbf0f37e1e6cfb2286a102dbfa518
A Belgrave <abelgrave95@gmail.com>
2019-02-28 16:16:22 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -17,9 +17,9 @@ def reformat_languages(languages)
     puts langs
     # new_hash[lang] = {type: "", style: [style]}
     langs.each do |lang, type|
-      if !new_hash.key?(lang)
-        new_hash[lang][:style]= []
-      end
+      # if !new_hash.key?(lang)
+      #   new_hash[lang][:style]= []
+      # end
       new_hash[lang] = type
       new_hash[lang][:style] << style
 
