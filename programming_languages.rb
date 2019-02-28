688e3f04b7c4bb1ea14f82d4eb85c84edd433004
A Belgrave <abelgrave95@gmail.com>
2019-02-28 15:31:45 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = Hash.new
+  languages.each do |style, lang|
+    if !new_hash.key?(lang.to_s)
+      new_hash[lang.to_s] = {type: "", style: []}
+    else
+      lang[:style] << style
+      lang.each do |type, tv|
+        new_hash[:type] << tv
+      end
+    end
+  end
+
+
 end
